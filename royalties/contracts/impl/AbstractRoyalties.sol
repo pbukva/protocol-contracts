6cfb654e872f61ce8f3bf1c5a39a57d83a1f9df3
Peter Bukva <peter.bukva@fetch.ai>
2022-02-26 10:08:39 +0000

    Fixing royalties contracts required for nft

--- royalties/contracts/impl/AbstractRoyalties.sol
+++ royalties/contracts/impl/AbstractRoyalties.sol
@@ -1,29 +1,42 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.2 <0.8.0;
+pragma solidity ^0.8.0;
 
 import "../LibPart.sol";
 
 abstract contract AbstractRoyalties {
+    uint96 constant denominator = 10000;
     mapping (uint256 => LibPart.Part[]) internal royalties;
 
     function _saveRoyalties(uint256 id, LibPart.Part[] memory _royalties) internal {
         uint256 totalValue;
-        for (uint i = 0; i < _royalties.length; i++) {
-            require(_royalties[i].account != address(0x0), "Recipient should be present");
-            require(_royalties[i].value != 0, "Royalty value should be positive");
-            totalValue += _royalties[i].value;
-            royalties[id].push(_royalties[i]);
+        LibPart.Part[] storage stor_royalties = royalties[id];
+
+        for (uint i = 0; i < stor_royalties.length; ++i) {
+            totalValue += stor_royalties[i].value;
         }
+
+        for (uint i = 0; i < _royalties.length; ++i) {
+            LibPart.Part memory _royalty = _royalties[i];
+            require(_royalty.account != address(0x0), "Recipient must be present");
+            require(_royalty.value != 0, "Royalty value must be positive");
+            require(_royalty.value < denominator, "Royalty value should be ");
+            totalValue += _royalty.value;
+            stor_royalties.push(_royalty);
+        }
+
         require(totalValue < 10000, "Royalty total value should be < 10000");
+
         _onRoyaltiesSet(id, _royalties);
     }
 
     function _updateAccount(uint256 _id, address _from, address _to) internal {
-        uint length = royalties[_id].length;
-        for(uint i = 0; i < length; i++) {
-            if (royalties[_id][i].account == _from) {
-                royalties[_id][i].account = address(uint160(_to));
+        LibPart.Part[] storage _royalties = royalties[_id];
+        uint length = _royalties.length;
+        for(uint i = 0; i < length; ++i) {
+            LibPart.Part storage royalty = _royalties[i];
+            if (royalty.account == _from) {
+                royalty.account = payable(_to);
             }
         }
     }
--- royalties/contracts/impl/RoyaltiesV1Impl.sol
+++ royalties/contracts/impl/RoyaltiesV1Impl.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.2 <0.8.0;
+pragma solidity ^0.8.0;
 
 import "./AbstractRoyalties.sol";
 import "../RoyaltiesV1.sol";
@@ -10,8 +10,8 @@ contract RoyaltiesV1Impl is AbstractRoyalties, RoyaltiesV1 {
     function getFeeRecipients(uint256 id) public override view returns (address payable[] memory) {
         LibPart.Part[] memory _royalties = royalties[id];
         address payable[] memory result = new address payable[](_royalties.length);
-        for (uint i = 0; i < _royalties.length; i++) {
-            result[i] = address(uint160(_royalties[i].account));
+        for (uint i = 0; i < _royalties.length; ++i) {
+            result[i] = _royalties[i].account;
         }
         return result;
     }
@@ -19,7 +19,7 @@ contract RoyaltiesV1Impl is AbstractRoyalties, RoyaltiesV1 {
     function getFeeBps(uint256 id) public override view returns (uint[] memory) {
         LibPart.Part[] memory _royalties = royalties[id];
         uint[] memory result = new uint[](_royalties.length);
-        for (uint i = 0; i < _royalties.length; i++) {
+        for (uint i = 0; i < _royalties.length; ++i) {
             result[i] = _royalties[i].value;
         }
         return result;
@@ -28,9 +28,10 @@ contract RoyaltiesV1Impl is AbstractRoyalties, RoyaltiesV1 {
     function _onRoyaltiesSet(uint256 id, LibPart.Part[] memory _royalties) override internal {
         address[] memory recipients = new address[](_royalties.length);
         uint[] memory bps = new uint[](_royalties.length);
-        for (uint i = 0; i < _royalties.length; i++) {
-            recipients[i] = _royalties[i].account;
-            bps[i] = _royalties[i].value;
+        for (uint i = 0; i < _royalties.length; ++i) {
+            LibPart.Part[] memory _royalty = _royalties[i];
+            recipients[i] = _royalty.account;
+            bps[i] = _royalty.value;
         }
         emit SecondarySaleFees(id, recipients, bps);
     }
